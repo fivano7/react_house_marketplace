d060dfd2fadd17a44b970073992e8300a5f9c155
fivano7 <filipivanovic7@gmail.com>
2022-11-28 13:22:27 +0100

    without geolocation for showing listing

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import CreateListing from './pages/CreateListing';
+import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -19,7 +21,14 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
+
+          {/* http://localhost:3000/category/rent/O78lZcaVGKTet5TGgybo */}
           <Route path='/category/:categoryName' element={<Category />} />
+
+          <Route
+            path='/category/:categoryName/:listingId'
+            element={<Listing />}
+          />
 
           {/* ovaj način je jednostavniji za skužit */}
           <Route
@@ -40,6 +49,10 @@ function App() {
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
+
+          {/*                        contact/landlordId?listingName */}
+          {/* http://localhost:3000/contact/8fPh7awdad3?listingName=Moder */}
+          <Route path='/contact/:landlordId' element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
